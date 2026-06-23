f22645e08313bf3d9604544cc36162a5b3b5b63e
Mockingbird-42 <grinchenko.ortho@gmail.com>
2026-06-23 16:23:00 +0300

    Add files via upload

---
+++ first_try.py
@@ -0,0 +1,11 @@
+x=int(input("How old are you?  "))
+if x < 18:
+    print("You just a child")
+else:
+    print("Ok")
+password = 387
+y=int(input("Insert password "))
+if y == 387:
+    print("access allowed")
+else:
+    print("access denied")
