881aff741dee0d575492c7e01689305a6de9f83e
Mockingbird-42 <grinchenko.ortho@gmail.com>
2026-07-05 13:43:46 +0300

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,8 @@
+x="aakncaevxh4faabhk2xgijbc4hvgkngkbv7yibcdjaaaak42dyjbfkjvdbjydvjaa"
+y=len(x) 
+z=chr(y)
+a=input("password: ")
+if a==z:
+    print("Good boy!")
+else:
+    print("Bad boy...")
---
+++ variables.py
@@ -0,0 +1,19 @@
+x=input("Your name: ")
+y=input("Your surname: ")
+z="Your email: "
+j="@babay.com"
+print(z+""+x+""+y+"@babay.com")
+print(f"Your email: {x}{y}@babay.com")
+print(f"{z}{x}{y}{j}")
+
+def a ():
+    b()
+    c()    
+def b(): print("\n\babadoo")
+def c(): print("doodooba")
+
+if x=="12":
+    a()
+else:
+    print("\nno!")
+
